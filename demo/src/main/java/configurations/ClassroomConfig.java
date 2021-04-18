48f77d7efd41dec02789dd514068cfb8152398b4
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-17 23:28:56 -0400

    classroomconfig made

---
+++ demo/src/main/java/configurations/ClassroomConfig.java
@@ -0,0 +1,28 @@
+package configurations;
+
+import classes.Classroom;
+import classes.Students;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class ClassroomConfig {
+
+    @Bean
+    @DependsOn({"students", "instructors"})
+    public Classroom currentCohort() {
+        StudentConfig studentConfig = new StudentConfig();
+        InstructorsConfig instructorsConfig = new InstructorsConfig();
+        return new Classroom(studentConfig.currentStudents(), instructorsConfig.zipcodeInstructors());
+    }
+
+    @Bean
+    @DependsOn({"previousStudents", "instructors"})
+    public Classroom previousCohort() {
+        StudentConfig studentConfig = new StudentConfig();
+        InstructorsConfig instructorsConfig = new InstructorsConfig();
+        return new Classroom(studentConfig.previousStudents(), instructorsConfig.zipcodeInstructors());
+    }
+
+}
